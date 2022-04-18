a59114f10c1080ae5da5c04f684c68df299eba7a
Luis Miguel Grajales <40398968+luismgluis@users.noreply.github.com>
2022-04-17 20:43:44 -0500

    change readme

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -6,5 +6,5 @@ const firebaseConfig = {
 	messagingSenderId: "1062751012575",
 	appId: "1:1062751012575:web:28fea5827d48ae5f780b6a",
 	measurementId: "G-WKCPMWD9J3",
-};
+}; // ss
 export default firebaseConfig;
